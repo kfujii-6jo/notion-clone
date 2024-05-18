7dad0cc6edcd3f96d233c98f78f4dddf4f4853e8
k.fujii <k.fujii.engr@gmail.com>
2024-05-19 02:59:41 +0900

    add user registration

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const mongoose = require("mongoose");
-
+const CryptoJS = require("crypto-js");
+const User = require("./src/v1/models/user");
 const app = express();
 const PORT = 3000;
+app.use(express.json());
 require("dotenv").config();
 
 try  {
@@ -15,13 +17,22 @@ try  {
 
 app.get('/', (req, res) => {
     res.send("Hello Express");
-})
+});
 
-// api
-// regist 
+// user regist 
+app.post('/register', async (req, res) => {
+    console.log(req.body);
+    try {
+        req.body.password = CryptoJS.SHA256(req.body.password).toString();
+        console.log(req.body)
+        const user = User.create(req.body);
+        return res.status(200).json({ message: 'Registration successful!' });
+    } catch (error) {
+        console.log(error);
+    }
+});
 
-
-// login api
+// user login
 
 app.listen(PORT, () => {
     console.log("start server...")
